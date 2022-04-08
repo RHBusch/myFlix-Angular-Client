3ead65ac4879a3bc445b04161ebbdca763892037
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-04-07 21:13:03 -0400

    Establishing prelim code to call APIs

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, catchError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+//Declaring api url providing data to the client app 
+const apiUrl = '';
+
+//Get token request from local storage for authentication
+const token = localStorage.getItem('token');
+
+//Get username request from local storage for use in URLs and auth
+const username = localStorage.getItem('username')
+
+//Injectable is an Angular decorator. This is stating that the service is available across the app. 
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FetchApiDataService {
+  // Inject the HttpClient module to the constructor params
+  // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {
+  }
+  // Making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'users', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+  //CREATE API CALLS 
+
+
+
+
+
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
